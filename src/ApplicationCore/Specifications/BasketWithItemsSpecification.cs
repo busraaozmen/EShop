8fd164e8f48b295019d51f864963048e00f43e81
busraaozmen <busra.ozmenn@outlook.com>
2020-06-17 22:16:28 +0300

    Basket Servis

---
+++ src/ApplicationCore/Specifications/BasketWithItemsSpecification.cs
@@ -0,0 +1,24 @@
+﻿using ApplicationCore.Entities;
+using Ardalis.Specification;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace ApplicationCore.Specifications
+{
+    public class BasketWithItemsSpecification : BaseSpecification<Basket>
+    {
+        public BasketWithItemsSpecification(int basketId) : base(x => x.Id == basketId)
+        {
+            
+            AddInclude(x => x.Items);
+        }
+        public BasketWithItemsSpecification(string buyerId) : base(x => x.BuyerId == buyerId)
+        {
+            AddInclude(x => x.Items);
+        }
+
+
+    }
+}
